c920304e29e781529196b945dc7f4a4a8cd4a092
Lawrentique <Tojota2612@gmail.com>
2026-02-16 14:49:28 +0200

    class work done
    :(

--- Zombie/Assets/Script/GameManager.cs
+++ Zombie/Assets/Script/GameManager.cs
@@ -1,18 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject selectedZombie;
     public GameObject[] zombies;
-   
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Vector3 selectedSize;
+    public Vector3 pushForce;
+    private InputAction next, prev, jump;
+    private int selectedIndex = 0;
+
     void Start()
     {
-        
+        next = InputSystem.actions.FindAction("NextZombie");
+        prev = InputSystem.actions.FindAction("PrevZombie");
+        jump = InputSystem.actions.FindAction("Jump");
+        SelectZombie(selectedIndex);
     }
 
-    // Update is called once per frame
+    void SelectZombie(int index)
+    {
+        if (selectedZombie != null)
+        {
+            selectedZombie.transform.localScale = Vector3.one;
+        }
+
+        selectedZombie = zombies[index];
+        selectedZombie.transform.localScale = selectedSize;
+        Debug.Log("selected: " + selectedZombie);
+    }
+
     void Update()
     {
-        
+        if (next.WasPressedThisFrame())
+        {
+            Debug.Log("next");
+            selectedIndex++;
+            if (selectedIndex >= zombies.Length)
+                selectedIndex = 0;
+            SelectZombie(selectedIndex);
+        }
+
+        if (prev.WasPressedThisFrame())
+        {
+            Debug.Log("prev");
+            selectedIndex--;
+            if (selectedIndex < 0)
+                selectedIndex = 3;
+            SelectZombie(selectedIndex);
+        }
+
+        if (jump.WasPressedThisFrame())
+        {
+            Debug.Log("jump");
+            Rigidbody rb = selectedZombie.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.AddForce(pushForce);
+        }
     }
 }
